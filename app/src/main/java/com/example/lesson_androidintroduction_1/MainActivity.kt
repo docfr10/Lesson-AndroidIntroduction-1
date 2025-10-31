b5a4ec6f9ec30bff9893180ad2182014e8efa73a
docfr10 <docfr10@yandex.ru>
2025-10-31 11:51:51 +0300

    Final

--- app/src/main/java/com/example/lesson_androidintroduction_1/MainActivity.kt
+++ app/src/main/java/com/example/lesson_androidintroduction_1/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.lesson_androidintroduction_1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,6 +26,44 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Создание текстового элемента UI в коде в виде объекта со ссылкой на xml элемент
+        val textView: TextView = findViewById<TextView>(R.id.textView)
+        val helloWorld = findViewById<TextView>(R.id.helloWorld)
+
+        // Создание кнопки
+        val button = findViewById<Button>(R.id.button)
+
+        // Создание изображения
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        // Взаимодействие с текстовым элементом — изменение текста
+        textView.text = "Новый текст"
+
+        // Взаимодействие с текстовым элементом — нажатие на текст
+        textView.setOnClickListener { // Слущатель нажатий
+            textView.text = "тскет йывоН"
+        }
+
+        // Нажатие на кнопку
+        button.setOnClickListener {
+            button.text = "Вы нажали на кнопку!"
+        }
+
+        var change = true
+
+        // Нажатие на картинку
+        imageView.setOnClickListener {
+            // Смена картинки
+
+            if (change) {
+                imageView.setImageResource(R.drawable.cat_with_tears_of_joy)
+                change = false
+            } else {
+                imageView.setImageResource(R.drawable.ac4db44e951664df88ea715ff9b5d462)
+                change = true
+            }
         }
     }
 }
